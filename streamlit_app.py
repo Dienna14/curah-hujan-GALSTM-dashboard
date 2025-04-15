67087b15b40c2fa6f3e613e21fd5a0a9a2901305
Dienna14 <113268664+Dienna14@users.noreply.github.com>
2025-04-15 13:46:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
